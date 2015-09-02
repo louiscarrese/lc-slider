e56e85b7060b6772fcee1b7bffd6f1662885abf5
Louis Carrese <louis.carrese@gmail.com>
2015-09-02 14:50:32 +0200

    Added the function to convert wordpress params to owl params

--- lc-slider.js
+++ lc-slider.js
@@ -1,11 +1,42 @@
 
 jQuery(function () {
-	jQuery('.owl-carousel').owlCarousel({
-		singleItem: true,
-		navigation: true
 
-	});
+	jQuery('.owl-carousel').each(function() {
+		//Extract options from the wordpress configuration
+		options = extractOptions(lcSliderParams, jQuery(this).data('sliderid'));
+
+		//Add common options
+		options['singleItem'] = true;
+		options['theme'] = 'ak-owl-theme';
+		options['navigationText'] = ['', ''];
+//		options['navigation'] = true;
+
+		//Init owl carousel
+		jQuery(this).owlCarousel(options);
+
+	}); 
+
+	// jQuery('.owl-carousel').owlCarousel({
+	// 	singleItem: true,
+	// 	navigation: true
+
+	// });
 
 });
 
-//init_lc_slider(lcSliderParams);
+
+/**
+ * Extract options from the wordpress stored format to this plugin format.
+ */
+function extractOptions(options, sliderId) {
+    return {
+    	navigation: (options[sliderId]["nav"]["arrows"] == 1)
+
+		// speed: options[sliderId]["speed"]["speed"],
+		// delay: options[sliderId]["speed"]["delay"],
+		// dots: (options[sliderId]["nav"]["bullets"] == 1),
+		// arrows: (options[sliderId]["nav"]["arrows"] == 1),
+		// progress: (options[sliderId]["eyecandy"]["progressbar"]),
+		// transitioned: options[sliderId]["eyecandy"]["transitioned"]
+    };
+}
